8342130c15c60898acd709d0df67b4d68811d02d
polhek <ziga.groselj@gmail.com>
2021-08-16 18:56:15 +0200

    started learning java

---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,12 @@
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+
+        int dividend = 3;
+        int divisor = 2;
+
+        double result = dividend / divisor * 1.0;
+        System.out.println(result);
+    }
+}
